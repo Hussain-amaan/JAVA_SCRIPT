5b6adcdcb5da67eaf5e08c3d945a98d339fdd961
Hussain-amaan <hussainaman347@gmail.com>
2026-08-08 23:35:12 +0530

    initial commit

--- JavaScript_Operators.js
+++ JavaScript_Operators.js
@@ -1,14 +1,14 @@
 //  What is the output?
 
-// let a = 10 ;
-// let b= 3;
+let a = 10 ;
+let b= 3;
 
 
-// console.log (a+b);
-// console.log(a-b);
-// console.log(a**b);
-// console.log(a/b);
-// console.log(a*b);
+console.log (a+b);
+console.log(a-b);
+console.log(a**b);
+console.log(a/b);
+console.log(a*b);
 
 
 
@@ -16,19 +16,19 @@
 
 //  What is the final value of x
 
-// let x =10
-// x+=5;
-// x*=2;
+let x =10
+x+=5;
+x*=2;
 
 
-// console.log(x)
+console.log(x)
 
 // **********************************************
 
 // What is the difference between the two results
 
-// console.log(5 == "5");
-// console.log(5 === "5");
+console.log(5 == "5");
+console.log(5 === "5");
 
 
 
@@ -36,3 +36,21 @@
 
 // What will each statement return?
 
+let age = 25;
+let hasID = true;
+
+console.log(age >18 && hasID);
+console.log(age < 18 && hasID);
+
+
+console.log(!hasID);
+
+
+
+// ******************************
+
+let a =5;
+
+console.log(a++);
+console.log(a);
+console.log(++a);
