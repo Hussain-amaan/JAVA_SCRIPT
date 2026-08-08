f8da8d08654e0373035872b52aa8bfd1b28ca648
Hussain-amaan <hussainaman347@gmail.com>
2026-08-08 23:27:07 +0530

    first commit

---
+++ JavaScript_Operators.js
@@ -0,0 +1,38 @@
+//  What is the output?
+
+// let a = 10 ;
+// let b= 3;
+
+
+// console.log (a+b);
+// console.log(a-b);
+// console.log(a**b);
+// console.log(a/b);
+// console.log(a*b);
+
+
+
+// ********************************************
+
+//  What is the final value of x
+
+// let x =10
+// x+=5;
+// x*=2;
+
+
+// console.log(x)
+
+// **********************************************
+
+// What is the difference between the two results
+
+// console.log(5 == "5");
+// console.log(5 === "5");
+
+
+
+// ********************************
+
+// What will each statement return?
+
---
+++ Variables and Data Types.js
@@ -0,0 +1,54 @@
+// 1. Variables
+
+// What will be the output?
+
+let name = "amaan"
+let age = 25 ;
+
+console.log("My Name is " + name  + "and i am "+ age + "years old");
+
+// ***********************************************************
+
+// 2. Data TypesIdentify the data type of each variable:
+
+let a = 25;
+let b = "25";
+let c = true;
+let d = null;
+let e;
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+
+// *******************************************
+
+
+// 3. let vs const
+
+// What happens when this code runs?
+
+
+
+let age =22
+age = 2
+console.log(age);
+console.log(name);
+
+// *********************************************
+
+// 4. Predict the Output What will this print?
+
+
+let x= "10"
+let y=5
+
+console.log(x+y)
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof (x+y))
+
