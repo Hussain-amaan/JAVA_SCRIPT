a24b595a4c6952e384bd44c4a48329a6885a6fbf
Hussain-amaan <hussainaman347@gmail.com>
2026-08-09 13:31:03 +0530

    Es6 Function

---
+++ L6_Es6Functions.js
@@ -0,0 +1,67 @@
+//  Normal Function
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(1,4))
+
+
+// Arrow Function
+
+// const add = (a,b) =>{
+//     return a+b;
+// }
+
+// console.log(add(1,2))
+
+
+// Arrow Function with One Parameter
+
+
+// const square = n =>{
+//     return n*n;
+// }
+
+
+// console.log(square(4))
+
+
+// Implicit Return
+// If your function has only one expression, you can remove {} and return
+
+
+
+// const add = (a,b) => a+b;
+// console.log(add(5, 10));
+
+
+// Arrow Function with Multiple Statements
+// If you need multiple statements, use {} and return explicitly:
+
+// const calculate = (a, b) => {
+//     let result = a + b;
+//     return result;
+// };
+
+// console.log(calculate(10, 20));
+
+
+// Default Parameters
+
+// function greet(name="user"){
+//     return "Hello " + name;
+// }
+
+// console.log(greet("amaan"))
+
+
+// Template Literals
+
+// const greet = name => `hello ${name}`;
+
+// let name = "amaan"
+
+// console.log(greet(name));
+
+
