2d3193e9691fd8272253d6a6fa3ebe9f05f2918c
Hussain-amaan <hussainaman347@gmail.com>
2026-08-09 23:37:33 +0530

    Es6 Functions

--- L6_Es6Functions.js
+++ L6_Es6Functions.js
@@ -1,30 +1,30 @@
 //  Normal Function
 
-// function add(a,b){
-//     return a+b;
-// }
+function add(a,b){
+    return a+b;
+}
 
-// console.log(add(1,4))
+console.log(add(1,4))
 
 
 // Arrow Function
 
-// const add = (a,b) =>{
-//     return a+b;
-// }
+const add = (a,b) =>{
+    return a+b;
+}
 
-// console.log(add(1,2))
+console.log(add(1,2))
 
 
 // Arrow Function with One Parameter
 
 
-// const square = n =>{
-//     return n*n;
-// }
+const square = n =>{
+    return n*n;
+}
 
 
-// console.log(square(4))
+console.log(square(4))
 
 
 // Implicit Return
@@ -32,36 +32,84 @@
 
 
 
-// const add = (a,b) => a+b;
-// console.log(add(5, 10));
+const add = (a,b) => a+b;
+console.log(add(5, 10));
 
 
 // Arrow Function with Multiple Statements
 // If you need multiple statements, use {} and return explicitly:
 
-// const calculate = (a, b) => {
-//     let result = a + b;
-//     return result;
-// };
+const calculate = (a, b) => {
+    let result = a + b;
+    return result;
+};
 
-// console.log(calculate(10, 20));
+console.log(calculate(10, 20));
 
 
 // Default Parameters
 
-// function greet(name="user"){
-//     return "Hello " + name;
-// }
+function greet(name="user"){
+    return "Hello " + name;
+}
 
-// console.log(greet("amaan"))
+console.log(greet("amaan"))
 
 
 // Template Literals
 
-// const greet = name => `hello ${name}`;
+const greet = name => `hello ${name}`;
 
-// let name = "amaan"
+let name = "amaan"
 
-// console.log(greet(name));
+console.log(greet(name));
 
 
+// Function Returning an Object
+
+const createUser = () => {
+    return {
+        name: "Amaan",
+        age: 22
+    };
+};
+
+console.log(createUser())
+
+
+const greet = (name = "Guest") => {
+    return `Hello ${name}`;
+};
+
+console.log(greet());
+console.log(greet("Amaan"));
+
+
+// Create an arrow function:
+
+// calculateArea(length, width)
+
+// Requirements:
+
+// If no length is provided, use 10
+// If no width is provided, use 5
+// Return the area
+
+
+function Area(length = 10, breadth = 5) {
+    let area = length * breadth;
+    return area;
+}
+
+console.log(Area());
+console.log(Area(10, 2));
+
+
+// With  ES6 arrow function
+
+const Area=(l=10,b=5)=>{
+    return l*b;
+};
+
+console.log(Area());
+console.log(Area(10, 2));
