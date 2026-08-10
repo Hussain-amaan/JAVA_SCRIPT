a7c3060c0213f5ccb53a9d69240ddd072f01b7eb
Hussain-amaan <hussainaman347@gmail.com>
2026-08-11 00:35:15 +0530

    Array

---
+++ L8_Array.js
@@ -0,0 +1,77 @@
+// JavaScript Arrays : An array lets you store multiple values inside one variable.
+
+// Creating an Array
+
+// let studnets=["amaan","anzar","aabish"]
+
+// console.log(studnets);
+
+// let data = ["Amaan", 22, true, 10.5];
+// console.log(data);
+
+// ************************************************************************
+
+// Array Index:
+// let fruits = ["Apple", "Banana", "Orange"];
+// console.log([fruits[0]])
+
+// ************************************************************************
+
+// Changing an Array Element : Arrays are mutable, meaning you can change their values.
+
+// let fruits = ["Apple", "Banana", "Orange"];
+
+// fruits[1] = "Mango";
+
+// console.log(fruits);
+
+// ************************************************************************
+
+// length :length tells you how many elements are in the array.
+
+// let fruits = ["Apple", "Banana", "Orange"];
+
+// console.log(fruits.length);
+
+// ************************************************************************
+
+// Adding Elements
+
+// push() : Adds an element to the end.
+
+// let fruits = ["Apple", "Banana"];
+
+// fruits.push("Orange");
+
+// console.log(fruits);
+
+// ##########################################################################
+
+// unshift(): Adds an element to the beginning.
+
+// let fruits = ["Apple", "Banana"];
+// fruits.unshift("Mango");
+// console.log(fruits);
+
+// ************************************************************************
+
+
+// Removing Elements :
+
+
+// pop()  : Removes the last element
+
+// let fruits = ["Apple", "Banana", "Orange"];
+
+// fruits.pop();
+
+// console.log(fruits);
+
+
+// ##########################################################################
+
+// shift() : Removes the first element.
+
+// let fruits = ["Apple", "Banana", "Orange"];
+// fruits.shift();
+// console.log(fruits);
