04f6419bb799ce467aa9f1c75807dd2a87e1c74d
Hussain-amaan <hussainaman347@gmail.com>
2026-08-10 23:30:18 +0530

    hof

--- L7_Higher_Order_Functions.js
+++ L7_Higher_Order_Functions.js
@@ -3,13 +3,57 @@
 
 // Function as an Argument
 
-function sayHello(){
-    console.log("HeLLO");
+// function sayHello(){
+//     console.log("HeLLO");
+// }
+
+// function execute (fn){
+//     fn();
+// }
+
+
+// execute(sayHello);
+
+
+
+// Function Returning Another Function
+
+// function outer() {
+
+//     function inner() {
+//         console.log("Hello!");
+//     }
+
+//     return inner;
+// }
+
+// const result = outer();
+
+// result();
+
+// Calculator
+
+function add(a,b){
+    let sum =a+b;
+    return sum;
 }
 
-function execute (fn){
-    fn();
+function sub(a,b){
+    return a-b;
 }
 
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
 
-execute(sayHello);
+function Calculator(a,b,operation){
+    return operation(a, b);
+}
+
+console.log(Calculator(1,2,add));
+console.log(Calculator(1,2,multiply));
+console.log(Calculator(1,2,sub));
+console.log(Calculator(1,2,divide));
