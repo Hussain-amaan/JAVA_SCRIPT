9945a58919d8b2993a5d7ef85aea6512109b0fb7
Hussain-amaan <hussainaman347@gmail.com>
2026-08-10 23:36:15 +0530

    hof

--- L7_Higher_Order_Functions.js
+++ L7_Higher_Order_Functions.js
@@ -3,33 +3,33 @@
 
 // Function as an Argument
 
-// function sayHello(){
-//     console.log("HeLLO");
-// }
+function sayHello(){
+    console.log("HeLLO");
+}
 
-// function execute (fn){
-//     fn();
-// }
+function execute (fn){
+    fn();
+}
 
 
-// execute(sayHello);
+execute(sayHello);
 
 
 
 // Function Returning Another Function
 
-// function outer() {
+function outer() {
 
-//     function inner() {
-//         console.log("Hello!");
-//     }
+    function inner() {
+        console.log("Hello!");
+    }
 
-//     return inner;
-// }
+    return inner;
+}
 
-// const result = outer();
+const result = outer();
 
-// result();
+result();
 
 // Calculator
 
@@ -52,6 +52,19 @@ function divide(a,b){
 function Calculator(a,b,operation){
     return operation(a, b);
 }
+
+console.log(Calculator(1,2,add));
+console.log(Calculator(1,2,multiply));
+console.log(Calculator(1,2,sub));
+console.log(Calculator(1,2,divide));
+
+
+const add=(a,b) =>a+b;
+const multiply=(a,b)=>a*b;
+const sub=(a,b)=> a-b;
+const divide=(a,b)=>a/b;
+
+const Calculator=(a,b,operation)=>operation(a,b);
 
 console.log(Calculator(1,2,add));
 console.log(Calculator(1,2,multiply));
