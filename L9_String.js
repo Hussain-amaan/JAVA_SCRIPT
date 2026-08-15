a8d78899f539f25eecff074aadec5c0bb47c4c8c
Hussain-amaan <hussainaman347@gmail.com>
2026-08-15 23:32:47 +0530

    Strings

--- L9_String.js
+++ L9_String.js
@@ -1,84 +1,84 @@
 // Print the first character and the last character.
 
-// let text = "JavaScript";
+let text = "JavaScript";
 
-// console.log(text[0]);
-// console.log(text[9])
+console.log(text[0]);
+console.log(text[9])
 
 // ******************************************
 // Find the length of the string.
 
-// let text = "JavaScript";
+let text = "JavaScript";
 
-// console.log(text.length);
+console.log(text.length);
 
 
 // **********************************
 // Convert it to uppercase.
 
 
-// let text = "JavaScript";
+let text = "JavaScript";
 
-// console.log(text.toUpperCase());
+console.log(text.toUpperCase());
 
 
 // ########################################
 // Convert it to lowercase.
 
-// let text = "JAVASCRIPT"
+let text = "JAVASCRIPT"
 
-// console.log(text.toLowerCase());
+console.log(text.toLowerCase());
 
 
 // ########################################
 // Remove the spaces from the beginning and end.
 
-// let text="   JAVASCRIPT   "
+let text="   JAVASCRIPT   "
 
-// console.log(text.trim())
+console.log(text.trim())
 
 
 // ################################################
 // Use charAt() to print the character at index 4.
 
 
-// let text = "Programming";
+let text = "Programming";
 
 
-// console.log(text.charAt(4));
+console.log(text.charAt(4));
 
 
 // ################################################
 // Check whether "JavaScript" exists in the string.
 
-// let text = "I am learning JavaScript";
+let text = "I am learning JavaScript";
 
-// console.log(text.includes("JavaScript"));
+console.log(text.includes("JavaScript"));
 
 // #################################################
 
 // Check whether the string starts with "JavaScript".
 
-// let text = "JavaScript is powerful";
-// console.log(text.startsWith("JavaScript"))
+let text = "JavaScript is powerful";
+console.log(text.startsWith("JavaScript"))
 
 
 // ##############################################
 
 // Combine Method
 
-// let corpus = "   hello javascript   ";
+let corpus = "   hello javascript   ";
 
 
-// function clean_text(text){
-//     text = text.trim();
-//     text = text.toUpperCase();
+function clean_text(text){
+    text = text.trim();
+    text = text.toUpperCase();
 
-//     return text
-// };
+    return text
+};
 
-// t1=clean_text(corpus);
-// console.log(t1);
+t1=clean_text(corpus);
+console.log(t1);
 
 // #########################################
 
