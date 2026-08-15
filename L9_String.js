1dbbf129f9f567cb8e2ed1f79de813e0bfa80901
Hussain-amaan <hussainaman347@gmail.com>
2026-08-15 23:31:15 +0530

    Strings

---
+++ L9_String.js
@@ -0,0 +1,85 @@
+// Print the first character and the last character.
+
+// let text = "JavaScript";
+
+// console.log(text[0]);
+// console.log(text[9])
+
+// ******************************************
+// Find the length of the string.
+
+// let text = "JavaScript";
+
+// console.log(text.length);
+
+
+// **********************************
+// Convert it to uppercase.
+
+
+// let text = "JavaScript";
+
+// console.log(text.toUpperCase());
+
+
+// ########################################
+// Convert it to lowercase.
+
+// let text = "JAVASCRIPT"
+
+// console.log(text.toLowerCase());
+
+
+// ########################################
+// Remove the spaces from the beginning and end.
+
+// let text="   JAVASCRIPT   "
+
+// console.log(text.trim())
+
+
+// ################################################
+// Use charAt() to print the character at index 4.
+
+
+// let text = "Programming";
+
+
+// console.log(text.charAt(4));
+
+
+// ################################################
+// Check whether "JavaScript" exists in the string.
+
+// let text = "I am learning JavaScript";
+
+// console.log(text.includes("JavaScript"));
+
+// #################################################
+
+// Check whether the string starts with "JavaScript".
+
+// let text = "JavaScript is powerful";
+// console.log(text.startsWith("JavaScript"))
+
+
+// ##############################################
+
+// Combine Method
+
+// let corpus = "   hello javascript   ";
+
+
+// function clean_text(text){
+//     text = text.trim();
+//     text = text.toUpperCase();
+
+//     return text
+// };
+
+// t1=clean_text(corpus);
+// console.log(t1);
+
+// #########################################
+
+
